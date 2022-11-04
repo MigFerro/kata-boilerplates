c3008aaf7a2e65e7f773ccec1506dc1ed3217e4f
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-11-04 11:39:24 +0000

    fix TS unit test

--- TypeScript/tests/unittests.spec.ts
+++ TypeScript/tests/unittests.spec.ts
@@ -4,7 +4,7 @@ import sum from '../src/index';
 describe('Example', () => {
 
     it('(fix me!) should sum properly', async () => {
-        expect(sum(1, 1)).toBeInstanceOf(3);
+        expect(sum(1, 1)).toEqual(3);
     });
 
 });
